8225fcaae8594204ad5b72998979a6f45c0801a4
green-c4-desktop <insertvalue@naver.com>
2025-02-11 11:00:56 +0900

    상세보기 UI

--- lib/data/_vm/post_list_view_model.dart
+++ lib/data/_vm/post_list_view_model.dart
@@ -4,7 +4,7 @@ import 'package:class_f_story/_core/utils/exception_handler.dart';
 import 'package:class_f_story/data/model/post_list.dart';
 import 'package:class_f_story/data/repository/post_repository.dart';
 import 'package:class_f_story/main.dart';
-import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
@@ -14,48 +14,88 @@ import '../../_core/utils/logger.dart';
 // 뷰 모델 객체를 계속 가지고 있을 필요가 없다.
 // AutoDisposeNotifier 를 사용해야 하는 이유를 알자 !!!!
 class PostListViewModel extends AutoDisposeNotifier<PostList?> {
-  // 상태관리가 아닌 멤버 변수
+  // RefreshController 사용하기로 함
   final refreshController = RefreshController();
-  final postRepository = const PostRepository();
   final mContext = navigatorkey.currentContext!;
+  final PostRepository postRepository = const PostRepository();
 
+  // 리버팟 2  - (Notifier)
   @override
   PostList? build() {
-    logger.d('PostListViewModel 초기화 메서드 호출 완료');
-
-    // 콜백 메서드 등록
-    ref.onDispose(() {
-      //  PostListViewModel가 메모리에서 내려갈 때 콜백 호출 ...
-      // refreshController 도 메모리 해제 처리
-      // 메모리 누수 방지
-      refreshController.dispose();
-    });
+    // 상태 초기화 코드
+    // 해야될 일 1. -> 콜백 메서드 등록 (메모리 해제 등록)
+    // onDispose 콜백 등록
+    ref.onDispose(
+      () {
+        logger.d("PostListViewModel 파괴시 실행됨");
+        refreshController.dispose(); // 가비지컬렉션이 바로 일어나지 않으니까!!
+      },
+    );
+    // 초기 init 메서드 호출
+    // API 통신 요청 처리
     init();
 
-    // 초기값 설정
     return null;
   }
 
-  // 게시글 목을 뿌리는 초기화 작업 - 행위
-  // 1. 예외처리
-  // 2. API 호출
-  // 3. success --> false 처리
-  // 3. success --> true 처리 ---> state 갱신
-  // 4. refreshController.loadCompleted() 호출 (동그라미 제거)
   Future<void> init() async {
     try {
+      // findAll -  {int page = 0}
       Map<String, dynamic> resBody = await postRepository.findAll();
       if (!resBody['success']) {
-        // 통신은 성공이지만 내부적으로 오류 본다 --> 적절한 에러 메세지를 던져 줌
         ExceptionHandler.handleException(
             resBody['errorMessage'], StackTrace.current);
-        return; // 실행의 제어권 반밥
+        return;
       }
+      // 상태 변경 (깊은 복사)
+      state = PostList.fromMap(resBody['response']);
 
-      // 통신 성공 -->
-      state = PostList.fromMap(resBody);
+      // RefreshController 콜백 메서드 호출
+      refreshController.refreshCompleted();
+    } catch (e, stackTrace) {
+      ExceptionHandler.handleException('게시글 목록 로딩 중 오류', stackTrace);
+    }
+  }
 
-      refreshController.loadComplete();
-    } catch (e, stackTrace) {}
+  // 페이징 처리 하는 로직 10 개 받은 상태이다. -->
+  // 1. 현재 상태값 가져오기 state
+  // 2. 마지막 페이지 여부를 확인 (
+  // 3. 서버에서 다음 페이지 요청 (0 --> 현재페이지 + 1)
+  // 4. 서버 응답 실패, 성공 여부 처리
+  // 5. 성공했다면 기존에 데이터에 + API 를 추가 해주면 된다.
+  // 6. 프로그래스바 종료 처리
+  Future<void> nextList() async {
+    PostList model = state!;
+
+    // 마지막 페이지가 true 라면
+    if (model.isLast) {
+      // UX 개선을 위해 딜레이 추가
+      await Future.delayed(Duration(milliseconds: 500));
+      refreshController.loadComplete(); // UI 갱신
+      return;
+    }
+
+    // API 통신 요청
+    Map<String, dynamic> responseBody =
+        await postRepository.findAll(page: state!.pageNumber + 1);
+
+    if (!responseBody['success']) {
+      ScaffoldMessenger.of(mContext!).showSnackBar(
+        SnackBar(content: Text('게시글을 못 불러 왔어요')),
+      );
+      return;
+    }
+
+    // 통신이 성공 이라면
+    PostList prevModel = state!;
+    PostList nextModel = PostList.fromMap(responseBody['response']);
+    state = nextModel.copyWith(posts: [...prevModel.posts, ...nextModel.posts]);
+    refreshController.loadComplete();
   }
 }
+
+// 창고 관리자 ( 창고 규격)
+final postListProvider =
+    NotifierProvider.autoDispose<PostListViewModel, PostList?>(
+  () => PostListViewModel(),
+);
