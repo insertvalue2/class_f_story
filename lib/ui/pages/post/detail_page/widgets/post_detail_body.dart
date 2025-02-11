4cbbbcdbfba8a2cd8473018635589932c83d6e12
green-c4-desktop <insertvalue@naver.com>
2025-02-11 11:43:58 +0900

    게시글 삭제 기능 구현

--- lib/ui/pages/post/detail_page/widgets/post_detail_body.dart
+++ lib/ui/pages/post/detail_page/widgets/post_detail_body.dart
@@ -1,54 +1,45 @@
+import 'package:class_f_story/ui/pages/post/detail_page/widgets/post_detail_buttons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../../_core/constants/size.dart';
+import '../../../../../data/_vm/post_detail_view_model.dart';
+import '../../../../../data/model/post_detail_model.dart';
 
-class PostDetailBody extends StatelessWidget {
+class PostDetailBody extends ConsumerWidget {
   final int postId;
   const PostDetailBody({required this.postId});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     // Future, Stream, State, Notifier
-    //PostDetailModel? model = ref.watch(postDetailProvider(postId));
+    PostDetail? model = ref.watch(postDetailProvider(postId));
+    if (model == null) {
+      return Center(child: CircularProgressIndicator());
+    }
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: ListView(
         children: [
           Text(
-            '게시글 1 (temp)',
+            '${model.post.title}',
             style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
           ),
           const SizedBox(height: largeGap),
           ListTile(
-            title: Text("사용자 이름"),
+            title: Text('${model.post.user!.username}'),
             leading: ClipRRect(
               borderRadius: BorderRadius.circular(50),
               child: Image.asset('assets/images/1.png', fit: BoxFit.cover),
             ),
           ),
           // 권한 처리 (sessionUser.id == post.user.id ) 같다면 보여 주기
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              IconButton(
-                onPressed: () {
-                  // 권한이 있다면 삭제 기능
-                },
-                icon: Icon(Icons.delete),
-              ),
-              IconButton(
-                onPressed: () {
-                  // 수정하기 화면으로 이동 처리
-                },
-                icon: Icon(CupertinoIcons.pen),
-              )
-            ],
-          ),
+          PostDetailButtons(model.post),
           const Divider(),
           const SizedBox(height: largeGap),
           SingleChildScrollView(
-            child: Text('게시글 내용 표시하기'),
+            child: Text('${model.post.content}'),
           )
         ],
       ),
